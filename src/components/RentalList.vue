5b0492b861e3c43710643c4c83e14c9b27e494c6
KyleVanDenHeuvel <vandenhk@mail.gvsu.edu>
2021-03-24 13:20:45 -0400

    Added skeleton code to components

---
+++ src/components/RentalList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="list">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RentalList',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
