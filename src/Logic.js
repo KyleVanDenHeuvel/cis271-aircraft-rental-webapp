8f1a88c7b42d21c85825c7c50f8ddcd42cfa880f
Philip Picard <emailmeforfreeipad@gmail.com>
2021-03-24 14:41:53 -0400

    Added logic file.

---
+++ src/Logic.js
@@ -0,0 +1,12 @@
+export default {
+    getAircraft: function () { },
+    getRentalsOnDate: function (date, aircraft) {
+        console.log(date);
+        console.log(aircraft);
+    },
+    addRental: function (aircraft, startTime, endTime) {
+        console.log(aircraft);
+        console.log(startTime);
+        console.log(endTime);
+    },
+};
