212c90a3ab26c52a1648a691a9e0ae3bd68efc60
Philip Picard <emailmeforfreeipad@gmail.com>
2021-03-24 14:05:04 -0400

    Created the login and create account pages.

--- src/components/CreateAccount.vue
+++ src/components/CreateAccount.vue
@@ -1,11 +1,20 @@
 <template>
-  <div></div>
+  <div id="login">
+    <h1>Create</h1>
+    Pilot Certificate Number: <input v-model="certificate" /><br />
+    Password: <input v-model="password" type="password" /><br />
+    Retype Password: <input v-model="password" type="password" /><br />
+    <button>Create!</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "CreateAccount",
   components: {},
+  methods: {
+    login: function () {},
+  },
 };
 </script>
 
--- src/components/LoginAccount.vue
+++ src/components/LoginAccount.vue
@@ -1,13 +1,25 @@
 <template>
-  <div></div>
+  <div id="login">
+    <h1>Login</h1>
+    Pilot Certificate Number: <input v-model="certificate" /><br />
+    Password: <input v-model="password" type="password" /><br />
+    <button>Login!</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "LoginAccount",
   components: {},
+  methods: {
+    login: function () {},
+  },
 };
 </script>
 
 <style>
+#login {
+  justify-content: left;
+  text-align: left;
+}
 </style>
