293db8c4900c390a6d68f63ba6b6445d24ec5f39
KyleVanDenHeuvel <vandenhk@mail.gvsu.edu>
2021-03-24 14:39:47 -0400

    Added basic styles to login page

--- src/components/LoginAccount.vue
+++ src/components/LoginAccount.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="login">
     <h1>Login</h1>
-    Pilot Certificate Number: <input v-model="certificate" /><br />
-    Password: <input v-model="password" type="password" /><br />
+    <p>Pilot Certificate Number: <input v-model="certificate" /></p>
+    <p>Password: <input v-model="password" type="password" /></p>
     <button>Login!</button>
   </div>
 </template>
@@ -21,5 +21,21 @@ export default {
 #login {
   justify-content: left;
   text-align: left;
+  margin: 2rem;
+  background-color: lightblue;
+}
+p {
+  margin-left: 1rem;
+}
+h1 {
+  padding: 1rem;
+}
+input {
+  border: 1px solid black;
+  border-radius: 3px;
+}
+button {
+  margin: 1rem;
+  font-size: 16px;
 }
 </style>
