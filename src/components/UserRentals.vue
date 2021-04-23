dd805f6c47f1078189a105867d5a0aa7b0a9835e
KyleVanDenHeuvel <vandenheuvelkr@gmail.com>
2021-04-23 15:08:53 -0400

    Retrieves data for user's rentals

--- src/components/UserRentals.vue
+++ src/components/UserRentals.vue
@@ -1,25 +1,55 @@
 <template>
   <div id="rentals">
-    <button v-on:click="logout()">Log Out</button>
-    <br />
-    <div>
-      <rental-list />
+    <div className="rentals-container">
+      <table>
+        <th>
+          <tr>
+            <td>Rentals</td>
+          </tr>
+        </th>
+
+        <tr v-for="(rental, pos) in userAircraft" :key="pos">
+          <td>Date: {{ rental.date }}</td>
+          <td>Time: {{ rental.time }}</td>
+          <td>Tail Number: {{ rental.tailNumber }}</td>
+          <td><button>X</button></td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-import RentalList from "./RentalList.vue";
-
 export default {
   name: "Weekly",
-  components: {
-    RentalList,
+  data: function() {
+    return {
+      userAircraft: [],
+    };
+  },
+
+  mounted: function() {
+    this.updateUserAircrafts();
   },
   methods: {
-    logout: function() {
-      this.$appAuth.signOut();
-      this.$router.back();
+    updateUserAircrafts: function() {
+      this.userAircraft = [];
+
+      this.$appDB
+        .collection("rentals")
+        .orderBy("date")
+        .where("pilot", "==", this.$appAuth.currentUser.uid)
+        .onSnapshot((qs) => {
+          this.userAircraft = [];
+          qs.forEach((qds) => {
+            if (qds.exists) {
+              // let data = qds.data();
+              // if (data.pilot == this.$appDB.currentUser.uid)
+              this.userAircraft.push(qds.data());
+            }
+          });
+        });
+      return this.userAircraft;
     },
   },
 };
@@ -30,6 +60,18 @@ export default {
   justify-content: left;
   text-align: left;
   margin: 2rem;
+  padding: 2rem;
   background-color: lightblue;
+}
+#rentals-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 2rem;
+}
+table {
+  border: 4px solid whitesmoke;
+  background-color: lightgray;
+  border-radius: 6px;
 }
 </style>
