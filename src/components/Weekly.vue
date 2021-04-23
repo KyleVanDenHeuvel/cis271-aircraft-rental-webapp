441e8225ae9574bf2bb2705ff640a126484a30b5
Philip Picard <emailmeforfreeipad@gmail.com>
2021-04-23 12:41:43 -0400

    Added TAF display and update HTML to use v-for.

--- src/components/Weekly.vue
+++ src/components/Weekly.vue
@@ -9,26 +9,14 @@
       <table id="calendar">
         <thead>
           <tr>
-            <th>{{ week[0].date }}</th>
-            <th>{{ week[1].date }}</th>
-            <th>{{ week[2].date }}</th>
-            <th>{{ week[3].date }}</th>
-            <th>{{ week[4].date }}</th>
-            <th>{{ week[5].date }}</th>
-            <th>{{ week[6].date }}</th>
+            <th v-for="day in week" v-bind:key="day.date">{{ day.date }}</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>
-              <p class="metar-data">{{ metar.data }}</p>
+            <td v-for="day in week" v-bind:key="day.date">
+              <p class="metar-data">{{ day.weather }}</p>
             </td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
           </tr>
         </tbody>
       </table>
@@ -51,13 +39,14 @@ export default {
     for (let i = 0; i < 7; i++) {
       week.push({
         date: currentDate.toLocaleDateString(),
+        weather: "",
       });
       currentDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
     }
 
-    let metar = { data: "" };
     const metarRequest = new XMLHttpRequest();
 
+    // Get today's most up-to-date weather conditions.
     metarRequest.open("GET", "https://avwx.rest/api/metar/KGRR");
     metarRequest.setRequestHeader(
       "Authorization",
@@ -66,14 +55,49 @@ export default {
 
     metarRequest.onreadystatechange = () => {
       if (metarRequest.readyState === 4) {
-        metar.data = JSON.parse(metarRequest.responseText).raw;
+        let metar = JSON.parse(metarRequest.responseText).raw;
+        // For the first day of the week, we will use the METAR instead
+        // of the TAF since we need to display real world conditions. We
+        // will also remove the remarks section because that takes
+        // up alot of space and it is not needed for a rental service.
+        week[0].weather = metar.split("RMK")[0];
       }
     };
     metarRequest.send();
+    const tafRequest = new XMLHttpRequest();
+
+    // Get the weather forecasts.
+    tafRequest.open("GET", "https://avwx.rest/api/taf/KGRR");
+    tafRequest.setRequestHeader(
+      "Authorization",
+      "Token fx4OxXj5kJNptfawyQMCjyhyXGJLukoV4qljp9stCjI"
+    );
+
+    tafRequest.onreadystatechange = () => {
+      if (tafRequest.readyState === 4) {
+        let data = JSON.parse(tafRequest.responseText);
+        for (let i = 0; i < data.forecast.length; i++) {
+          // Look at the time for the forecast and see if
+          // it is on the next day, if it is, then we will
+          // put it in the next day column.
+          let forecast = data.forecast[i];
+          let startTime = new Date(forecast.start_time.dt);
+          week.forEach((day) => {
+            // Check to see if the days match and then insert in
+            // the weather data for that day. We also need to make
+            // sure that we skip everything from the first day since
+            // that will have the metar.
+            if (day.date === startTime.toLocaleDateString() && day !== week[0])
+              day.weather = forecast.raw;
+          });
+        }
+      }
+    };
+
+    tafRequest.send();
 
     return {
       week: week,
-      metar: metar,
     };
   },
   methods: {
@@ -87,6 +111,7 @@ export default {
 
 <style>
 .metar-data {
+  min-width: 120pt;
   max-width: 120pt;
   font-size: 8pt;
 }
