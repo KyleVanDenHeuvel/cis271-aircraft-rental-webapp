8e7f4dc84fe971c7df1e4d61b3347ef73be86d60
KyleVanDenHeuvel <vandenheuvelkr@gmail.com>
2021-04-23 13:03:01 -0400

    Added props and binding to RentalItem

--- src/components/RentalItem.vue
+++ src/components/RentalItem.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="list">
-    
+    <tr>
+      <td><img v-bind:src="{ image }" alt="Aircraft Image" /></td>
+      <td>{{ aircraftType }}</td>
+      <td>{{ tailNumber }}</td>
+      <button>>>></button>
+    </tr>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'RentalItem',
+  name: "RentalItem",
   props: {
+    aircraftType: String,
     tailNumber: Number,
-    image: String
-  }
-}
+    image: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
