cd9b8522bba1a615ee77633d0faa4e7d66c2c5a8
Philip Picard <emailmeforfreeipad@gmail.com>
2021-04-23 13:21:36 -0400

    Added rental aircraft database updating.

--- src/components/Weekly.vue
+++ src/components/Weekly.vue
@@ -1,11 +1,9 @@
 <template>
   <div id="weekly">
     <button v-on:click="logout()">Log Out</button>
-    Aircraft:
-    <select></select>
     <br />
     <div id="week-container">
-      <rental-list />
+      <rental-list :rentals="aircraft" />
       <table id="calendar">
         <thead>
           <tr>
@@ -32,78 +30,104 @@ export default {
   components: {
     RentalList,
   },
-  data: () => {
-    let currentDate = new Date();
-    let week = [];
-    // Build a list of the next seven days.
-    for (let i = 0; i < 7; i++) {
-      week.push({
-        date: currentDate.toLocaleDateString(),
-        weather: "",
-      });
-      currentDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
-    }
-
-    const metarRequest = new XMLHttpRequest();
-
-    // Get today's most up-to-date weather conditions.
-    metarRequest.open("GET", "https://avwx.rest/api/metar/KGRR");
-    metarRequest.setRequestHeader(
-      "Authorization",
-      "Token fx4OxXj5kJNptfawyQMCjyhyXGJLukoV4qljp9stCjI"
-    );
-
-    metarRequest.onreadystatechange = () => {
-      if (metarRequest.readyState === 4) {
-        let metar = JSON.parse(metarRequest.responseText).raw;
-        // For the first day of the week, we will use the METAR instead
-        // of the TAF since we need to display real world conditions. We
-        // will also remove the remarks section because that takes
-        // up alot of space and it is not needed for a rental service.
-        week[0].weather = metar.split("RMK")[0];
-      }
-    };
-    metarRequest.send();
-    const tafRequest = new XMLHttpRequest();
-
-    // Get the weather forecasts.
-    tafRequest.open("GET", "https://avwx.rest/api/taf/KGRR");
-    tafRequest.setRequestHeader(
-      "Authorization",
-      "Token fx4OxXj5kJNptfawyQMCjyhyXGJLukoV4qljp9stCjI"
-    );
-
-    tafRequest.onreadystatechange = () => {
-      if (tafRequest.readyState === 4) {
-        let data = JSON.parse(tafRequest.responseText);
-        for (let i = 0; i < data.forecast.length; i++) {
-          // Look at the time for the forecast and see if
-          // it is on the next day, if it is, then we will
-          // put it in the next day column.
-          let forecast = data.forecast[i];
-          let startTime = new Date(forecast.start_time.dt);
-          week.forEach((day) => {
-            // Check to see if the days match and then insert in
-            // the weather data for that day. We also need to make
-            // sure that we skip everything from the first day since
-            // that will have the metar.
-            if (day.date === startTime.toLocaleDateString() && day !== week[0])
-              day.weather = forecast.raw;
-          });
-        }
-      }
-    };
-
-    tafRequest.send();
-
+  data: function () {
     return {
-      week: week,
+      aircraft: [],
+      week: [],
     };
+  },
+  mounted: function () {
+    this.updateAircrafts();
+    this.updateWeather();
   },
   methods: {
     logout: function () {
       this.$appAuth.signOut();
       this.$router.back();
+    },
+
+    // Fills out the weeks and updates it with weather data.
+    updateWeather: function () {
+      let currentDate = new Date();
+      this.week = [];
+      // Build a list of the next seven days.
+      for (let i = 0; i < 7; i++) {
+        this.week.push({
+          date: currentDate.toLocaleDateString(),
+          weather: "",
+        });
+        currentDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
+      }
+
+      const metarRequest = new XMLHttpRequest();
+
+      // Get today's most up-to-date weather conditions.
+      metarRequest.open("GET", "https://avwx.rest/api/metar/KGRR");
+      metarRequest.setRequestHeader(
+        "Authorization",
+        "Token fx4OxXj5kJNptfawyQMCjyhyXGJLukoV4qljp9stCjI"
+      );
+
+      metarRequest.onreadystatechange = () => {
+        if (metarRequest.readyState === 4) {
+          let metar = JSON.parse(metarRequest.responseText).raw;
+          // For the first day of the week, we will use the METAR instead
+          // of the TAF since we need to display real world conditions. We
+          // will also remove the remarks section because that takes
+          // up alot of space and it is not needed for a rental service.
+          this.week[0].weather = metar.split("RMK")[0];
+        }
+      };
+      metarRequest.send();
+      const tafRequest = new XMLHttpRequest();
+
+      // Get the weather forecasts.
+      tafRequest.open("GET", "https://avwx.rest/api/taf/KGRR");
+      tafRequest.setRequestHeader(
+        "Authorization",
+        "Token fx4OxXj5kJNptfawyQMCjyhyXGJLukoV4qljp9stCjI"
+      );
+
+      tafRequest.onreadystatechange = () => {
+        if (tafRequest.readyState === 4) {
+          let data = JSON.parse(tafRequest.responseText);
+          for (let i = 0; i < data.forecast.length; i++) {
+            // Look at the time for the forecast and see if
+            // it is on the next day, if it is, then we will
+            // put it in the next day column.
+            let forecast = data.forecast[i];
+            let startTime = new Date(forecast.start_time.dt);
+            this.week.forEach((day) => {
+              // Check to see if the days match and then insert in
+              // the weather data for that day. We also need to make
+              // sure that we skip everything from the first day since
+              // that will have the metar.
+              if (
+                day.date === startTime.toLocaleDateString() &&
+                day !== this.week[0]
+              )
+                day.weather = forecast.raw;
+            });
+          }
+        }
+      };
+
+      tafRequest.send();
+    },
+
+    updateAircrafts: function () {
+      this.aircraft = [];
+
+      this.$appDB
+        .collection("aircrafts")
+        .orderBy("type")
+        .onSnapshot((qs) => {
+          this.aircraft = [];
+          qs.forEach((qds) => {
+            if (qds.exists) this.aircraft.push(qds.data());
+          });
+        });
+      return this.aircraft;
     },
   },
 };
